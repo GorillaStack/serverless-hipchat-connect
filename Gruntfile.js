15c979801492aaca80d145fad9c83fd688b15cf2
Elliott Spira <elliottspira@gmail.com>
2016-05-04 16:57:43 +1000

    adding grunt workers to copy client source to appropriate directory to deploy client resources to s3

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'client/src/',
+        src: '**',
+        dest: 'client/dist/'
+      },
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('default', ['uglify']);
+};
