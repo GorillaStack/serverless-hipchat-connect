c5c5f9c9fdd6f23d10f2cd5f59b850dd06616bfa
Elliott Spira <elliottspira@gmail.com>
2016-05-05 12:32:24 +1000

    using grunt to transpile es6 to es5 for lambda. gitignoring generated es5 directory. adding serverless ofline package

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,16 @@ module.exports = function(grunt) {
         src: '**',
         dest: 'client/dist/'
       },
+    },
+    run: {
+      babel: {
+        exec: 'node node_modules/babel-cli/bin/babel --presets es2015 -d restApi/lib --watch restApi/src'
+      },
     }
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-run');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.registerTask('default', ['copy']);
 };
