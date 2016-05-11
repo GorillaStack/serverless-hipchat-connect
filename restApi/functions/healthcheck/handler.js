e8bba7f15320799e53d1d1f3cc65365a777be986
Elliott Spira <elliottspira@gmail.com>
2016-05-11 15:38:42 +1000

    removing usage of optimizer plugin as it was breaking includes from lib/

--- restApi/functions/healthcheck/handler.js
+++ restApi/functions/healthcheck/handler.js
@@ -1,12 +1,10 @@
 'use strict';
 
-import lib from '../../lib';
+const lib = require('../../lib/index').default();
 
-const handler = (event, context, cb) => {
-  lib.logger.log('debug', 'In healthcheck handler');
+exports.handler = function (event, context, cb) {
+  lib.logger.log('debug', 'In /healthcheck handler');
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
 };
-
-export {handler};
--- restApi/functions/root/handler.js
+++ restApi/functions/root/handler.js
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
+const lib = require('../../lib/index').default();
+
+exports.handler = function (event, context, cb) {
+  lib.logger.log('debug', 'In / handler');
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
