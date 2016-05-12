8f59e3cbe6fd8f9577446137a6be3f52cc2d347d
Elliott Spira <elliottspira@gmail.com>
2016-05-12 10:31:07 +1000

    adding package.json under the restApi directory to add dependencies to be deployed to lambda. moving config.json and atlassian-connect.json to folder for deployment to lambda

---
+++ restApi/src/dynamoDbManager.js
@@ -0,0 +1,10 @@
+/**
+* dynamoDbManager.js
+*
+* - Handle reads and writes to different DynamoDB tables
+* @exports an object of functions, each of which with business logic
+*/
+
+// 3rd Party Imports
+
+// Constants
