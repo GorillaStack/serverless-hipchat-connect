93a28d856752633bd3024392442d60f1b5611d7b
Elliott Spira <elliottspira@gmail.com>
2016-05-17 14:53:10 +1000

    adding handler code and configuration to handle glance requests

--- restApi/functions/glance-data/handler.js
+++ restApi/functions/glance-data/handler.js
@@ -1,7 +1,30 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+// Includes
+const lib = require('../../lib/index').default();
+const validateJWT = require('../../lib/jwt').validateJWT;
+
+const handleError = (err, cb) => {
+  lib.logger.error('error', 'Could not run /glance-data lambda', err);
+  cb(err);
+};
+
+module.exports.handler = function (event, context, cb) {
+  lib.logger.log('debug', 'In /glance-data handler');
+  lib.logger.log('debug', 'Event json:', JSON.stringify(event));
+
+  return validateJWT(event, lib).then(
+    (data) => {
+      let sampleGlanceData = {
+        label: {
+          value: '<b>Hello</b> World',
+          type: 'html'
+        }
+      };
+
+      cb(null, JSON.stringify(sampleGlanceData));
+    },
+
+    (err) => handleError(err, cb)
+  );
 };
